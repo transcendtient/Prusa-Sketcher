b5c20eb54a6febf516ec0718a035866109c90da3
transcendtient <transcendtient@gmail.com>
2014-07-03 08:47:10 -0500

    | tee log.txt for setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ Off='\e[0m'       # Text Reset
 Green='\e[0;32m'        # Green
 Red='\e[0;31m'          # Red
 
+rm ./log.txt
+
 #Java 1.7+
 #if we have java in path
 if type -p java; then
@@ -20,18 +22,18 @@ if [[ "$_java" ]]; then
   version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
 #if we found 1.7+ we're done
   if [[ "$version" > "1.7" ]]; then
-    echo -e ${Green}"\tYour installed Java version is 1.7+. Not installing Java."${Off}
+    echo -e ${Green}"\tYour installed Java version is 1.7+. Not installing Java."${Off} | tee -a log.txt 
 #else check the distro repo default version
   else         
-    echo -e ${Red}"\tYour installed Java version is < 1.7."${Off}
+    echo -e ${Red}"\tYour installed Java version is < 1.7."${Off} | tee -a log.txt 
     _DEF_JAVA=$(apt-cache policy default-jdk 2>&1 | grep Candidate | awk -F ':' '{print $3}')
 #repo sufficient then install it
     if [[ "$_DEF_JAVA" > "1.7" ]]; then
-      echo -e ${Green}"\tYour default Java version is $_DEF_JAVA. Installing default."${Off}
+      echo -e ${Green}"\tYour default Java version is $_DEF_JAVA. Installing default."${Off} | tee -a log.txt 
       sudo apt-get install default-jdk
 #else try to install manually
     else         
-      echo -e ${Red}"\tYour default Java version is < 1.7. Trying to install manually."${Off}
+      echo -e ${Red}"\tYour default Java version is < 1.7. Trying to install manually."${Off} | tee -a log.txt 
       sudo apt-get install openjdk-7-jre
     fi
   fi
@@ -40,11 +42,11 @@ else
   _DEF_JAVA=$(apt-cache policy default-jdk 2>&1 | grep Candidate | awk -F ':' '{print $3}')
 #repo sufficient then install it
   if [[ "$_DEF_JAVA" > "1.7" ]]; then
-    echo -e ${Green}"\tYour default Java version is $_DEF_JAVA. Installing default."${Off}
+    echo -e ${Green}"\tYour default Java version is $_DEF_JAVA. Installing default."${Off} | tee -a log.txt 
     sudo apt-get install default-jdk
 #else try to install manually
   else         
-    echo -e ${Red}"\tYour default Java version is < 1.7. Trying to install manually."${Off}
+    echo -e ${Red}"\tYour default Java version is < 1.7. Trying to install manually."${Off} | tee -a log.txt 
     sudo apt-get install openjdk-7-jre
   fi
 fi
@@ -59,14 +61,14 @@ if [[ "$_openscad" ]]; then
   version=$("$_openscad" -version 2>&1 | awk -F ' ' '{print $3}')
 #if sufficient we're done
   if [[ "$version" > "2013.06" ]]; then
-    echo -e ${Green}"\tYour OpenSCAD version is 2013.06+."${Off}
+    echo -e ${Green}"\tYour OpenSCAD version is 2013.06+."${Off} | tee -a log.txt 
 #else check the distro repo default
   else         
-    echo -e ${Red}"\tYour OpenSCAD version is < 2013.06."${Off}
+    echo -e ${Red}"\tYour OpenSCAD version is < 2013.06."${Off} | tee -a log.txt 
     _DEF_OPENSCAD=$(apt-cache policy default-jdk 2>&1 | grep Candidate | awk -F ':' '{print $2}')
 #if sufficient install it
     if [[ "$_DEF_OPENSCAD" > "2013.06" ]]; then
-      echo -e ${Green}"\tYour default OpenSCAD version is $_DEF_OPENSCAD. Installing..."${Off}
+      echo -e ${Green}"\tYour default OpenSCAD version is $_DEF_OPENSCAD. Installing..."${Off} | tee -a log.txt 
       sudo apt-get install openscad
     fi
   fi
@@ -75,21 +77,21 @@ else
   _DEF_OPENSCAD=$(apt-cache policy default-jdk 2>&1 | grep Candidate | awk -F ':' '{print $2}')
 #if the distro repo default is sufficient, install it
   if [[ "$_DEF_OPENSCAD" > "2013.06" ]]; then
-    echo -e ${Green}"\tYour default OpenSCAD version is $_DEF_OPENSCAD. Installing..."${Off}
+    echo -e ${Green}"\tYour default OpenSCAD version is $_DEF_OPENSCAD. Installing..."${Off} | tee -a log.txt 
     sudo apt-get install openscad
 #else we're BUILDING from source
   else         
-    echo -e ${Red}"\tYour default OpenSCAD version is < 2013.06. \n\tCompiling from source. This will take a long time!"${Off}
+    echo -e ${Red}"\tYour default OpenSCAD version is < 2013.06. \n\tCompiling from source. This will take a long time!"${Off} | tee -a log.txt 
 #if we already have it...    
     if [ -d "./openscad/" ]; then
-	  echo -e ${Green}"\tOpenSCAD directory present. Will not git."${Off}
+	  echo -e ${Green}"\tOpenSCAD directory present. Will not git."${Off} | tee -a log.txt 
 	else
       sudo apt-get install git
       git clone git://github.com/openscad/openscad.git
     fi
     cd openscad
     git submodule update --init
-    echo -e ${Green}"Getting dependencies."${Off}
+    echo -e ${Green}"Getting dependencies."${Off} | tee -a log.txt 
     sudo ./scripts/uni-get-dependencies.sh
 #parse fields from the check dependencies script
     ./scripts/check-dependencies.sh
@@ -100,7 +102,7 @@ else
 #if NotOK, build one at a time
     for (( i=1; i <= ${#isOK[@]}; i++ )); do
       if [[ ${isOK[i]}  == "NotOK" ]]; then
-        echo -e ${Red}"OpenSCAD dependencies not satisfied by script. \nBuilding ${nameDep[$i]} from source. This will take a long time!"${Off}
+        echo -e ${Red}"OpenSCAD dependencies not satisfied by script. \nBuilding ${nameDep[$i]} from source. This will take a long time!"${Off} | tee -a log.txt 
         source ./scripts/setenv-unibuild.sh
         ./scripts/uni-build-dependencies.sh {nameDep[$i]}
         sleep 10
@@ -115,10 +117,10 @@ else
     for (( i=1; i <= ${#isOK[@]}; i++ )); do
 #if the script fails, quit
       if [[ ${isOK[i]}  == "NotOK" ]]; then
-        echo -e ${Red}"OpenSCAD dependencies unable to be satisfied by script. \nABORTING!!"${Off}
+        echo -e ${Red}"OpenSCAD dependencies unable to be satisfied by script. \nABORTING!!"${Off} | tee -a log.txt 
         exit
       fi
-      echo -e ${Red}"Making! This will take a long time!"${Off}
+      echo -e ${Red}"Making! This will take a long time!"${Off} | tee -a log.txt 
       qmake
       make
       make install
@@ -131,13 +133,13 @@ fi
 #KISSlicer get kernel type
 _DEF_KERNEL=$(uname -m)
 if [[ "$_DEF_KERNEL" == "x86_64" ]]; then
-    echo -e ${Green}"\tUnzipping x64 KISSlicer."${Off}
+    echo -e ${Green}"\tUnzipping x64 KISSlicer."${Off} | tee -a log.txt 
     unzip ./Dependencies/KISSlicer_Linux64_1_1_0.zip -d ./kiss/
 elif [[ "$_DEF_KERNEL" == "i686" ]]; then
-    echo -e ${Green}"\tUnzipping i686 KISSlicer."${Off}
+    echo -e ${Green}"\tUnzipping i686 KISSlicer."${Off} | tee -a log.txt 
     unzip ./Dependencies/KISSlicer_Linux32_1_1_0.zip -d ./kiss/
 else         
-    echo -e ${Red}"\tYour kernel is unidentified. \n\tPlease take a look at the KISSlicer downloads section."${Off}
+    echo -e ${Red}"\tYour kernel is unidentified. \n\tPlease take a look at the KISSlicer downloads section."${Off} | tee -a log.txt 
     if which xdg-open > /dev/null; then
       xdg-open http://www.kisslicer.com/download.html 2>/dev/null
     elif which gnome-open > /dev/null; then
@@ -145,24 +147,28 @@ else
     fi
 fi
 
-echo -e ${Green}"\tInstalling Printrun dependencies."${Off}
+echo -e ${Green}"\tInstalling Printrun dependencies."${Off} | tee -a log.txt 
 sudo apt-get install python-serial python-wxgtk2.8 python-pyglet python-tornado python-setuptools python-libxml2 python-gobject avahi-daemon libavahi-compat-libdnssd1 python-dbus python-psutil git
 if [ -d "./Printrun/" ]; then
-	  echo -e ${Green}"\tPrintrun directory present. Will not git."${Off}
+	  echo -e ${Green}"\tPrintrun directory present. Will not git."${Off} | tee -a log.txt 
 else
-git clone https://github.com/kliment/Printrun.git
+    echo -e ${Green}"\tPrintrun directory not present. Will git."${Off} | tee -a log.txt 
+    git clone https://github.com/kliment/Printrun.git
 fi
 
-echo -e ${Green}"\tInstalling core dependencies."${Off}
+echo -e ${Green}"\tInstalling core dependencies."${Off} | tee -a log.txt 
 sudo apt-get install gimp imagemagick pstoedit librxtx-java potrace
 
-echo -e ${Green}"\tCompiling java programs."${Off}
+echo -e ${Green}"\tCompiling java programs."${Off} | tee -a log.txt 
 javac -classpath .:/usr/share/java/RXTXcomm.jar ./core/java/*.java
 
 #copy GIMP sccript
 if [ -d "/usr/share/gimp/2.0/scripts/" ]; then
-  echo -e ${Green}"\tCopying GIMP script! It's over!"${Off}
+  echo -e ${Green}"\tCopying GIMP script! It's over!"${Off} | tee -a log.txt 
   sudo cp ./Dependencies/do-pencil-drawing.scm /usr/share/gimp/2.0/scripts/
   sudo chmod +x /usr/share/gimp/2.0/scripts/do-pencil-drawing.scm
-else echo ${Red}"\tThe GIMP plugin directory was not found! Copy ./Dependencies/do-pencil-drawing.scm to your plugin directory to finish installation!"${Off}
+else echo -e ${Red}"\tThe GIMP plugin directory was not found! Copy ./Dependencies/do-pencil-drawing.scm to your plugin directory to finish installation!"${Off} | tee -a log.txt 
 fi
+
+echo -e "\n\nHere's a log of the script activity."
+cat log.txt
